979cccc41e01564d154cf6ac767f7bf2c6251d38
SeelamSravani17 <eww1cob@bosch.com>
2025-08-25 18:53:11 +0530

    Create Main_Test.cpp

---
+++ Main_Test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+void testTshirtSize();
+void testPrintColorMap();
+void testWeatherReport();
+
+TEST(TShirt, Size) {
+    testTshirtSize();
+}
+
+TEST(ColorMap, Print) {
+    testPrintColorMap();
+}
+
+TEST(WeatherReport, Report) {
+    testWeatherReport();
+}
